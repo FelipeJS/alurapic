2f32ebc973861895113eeeeaddb4183b4ef62fc3
felipejs <tecnologiagrave@gmail.com>
2020-02-10 21:23:23 -0300

    fim dos fundamentos

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -5,15 +5,24 @@ import { FilterByDescription } from "./filter-by-description-pipe";
 import { CommonModule } from "@angular/common";
 import { PhotoModule } from "../photo/photo.module";
 import { PhotosComponent } from "./photos/photos.component";
+import { CardModule } from "src/app/shared/components/card/card.module";
+import { SearchComponent } from "./search/search.component";
+import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-on-hover.module";
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent
     ],
-    imports: [CommonModule, PhotoModule]
+    imports: [
+        CommonModule,
+        PhotoModule,
+        CardModule,
+        DarkenOnHoverModule
+    ]
 })
 export class PhotoListModule {
 
--- src/app/photos/photos.mudule.ts
+++ src/app/photos/photos.mudule.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { PhotoFormModule } from "./photo-form/photo-form.module";
 import { PhotoListModule } from "./photo-list/photo-list.module";
 import { PhotoModule } from "./photo/photo.module";
+import { CardModule } from "../shared/components/card/card.module";
 
 @NgModule({
     declarations: [
